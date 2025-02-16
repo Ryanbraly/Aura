361d0c255f58fc46e67d928b688eb498d09ab544
Ryanbraly <rbralydev@gmail.com>
2025-02-15 23:27:42 -0600

    Swapped folders following VS corruption

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright my own damn self, it's just a learning project
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright my own damn self, it's just a learning project
+
+#pragma once
+
+#include "CoreMinimal.h"
+
