76785decc616abcbe4c60ad98bac0ec965ca9e2f
Ryanbraly <rbralydev@gmail.com>
2025-02-20 02:06:23 -0600

    Player movement implemented, groundwork for outlining

---
+++ Source/Aura/Private/Interaction/EnemyInterface.cpp
@@ -0,0 +1,6 @@
+// Copyright my own damn self, it's just a learning project
+
+
+#include "Interaction/EnemyInterface.h"
+
+// Add default functionality here for any IEnemyInterface functions that are not pure virtual.
