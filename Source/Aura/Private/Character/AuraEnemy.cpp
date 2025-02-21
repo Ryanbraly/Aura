18c8c54a75d771eb209317affe4548cce090f72a
Ryanbraly <rbralydev@gmail.com>
2025-02-21 02:06:40 -0600

    Replication setup, etc

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -1,7 +1,18 @@
 // Copyright my own damn self, it's just a learning project
 
-
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
 #include "Character/AuraEnemy.h"
+
+#include "AbilitySystem/AuraAttributeSet.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent -> SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+
+	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+}
 
 void AAuraEnemy::HighlightActor()
 {
